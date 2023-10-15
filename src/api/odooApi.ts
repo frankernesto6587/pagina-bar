27ae99c5718de47861bf1355546bdec31743f099
unknown <frankernesto6587@gmail.com>
2023-10-15 03:50:48 -0400

    add github actions

--- src/api/odooApi.ts
+++ src/api/odooApi.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import config from '../config'
 
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
 const api = axios.create({baseURL:config.api_url})
 export default api
 
